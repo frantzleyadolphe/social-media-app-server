7b8aebcd21700d32e01466f6a1b4636376e6cb9a
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-12-20 18:03:31 -0500

    function login created

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -20,26 +21,50 @@ export const register = async (req, res) => {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: "User already exists" });
-    } else {
-      // Hash the password
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
-
-      const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        password: passwordHash,
-        picturePath,
-        friends,
-        location,
-        occupation,
-        viewedProfile: Math.floor(Math.random() * 10000),
-        impressions: Math.floor(Math.random() * 10000),
-      });
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
     }
+    // Hash the password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/*LOGIN FUNCTION */
+export const login = async (req, res) => {
+  try {
+    //recuparation des donnees util isateur a travers la requete
+    const { email, password } = req.body;
+
+    //Verification de l'utilisateur dans la base de données
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
