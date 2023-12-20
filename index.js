fb1d0c07dcb8148d81735910d7e40d81be112e20
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-12-20 14:36:37 -0500

    new section added

--- index.js
+++ index.js
@@ -37,4 +37,19 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Port: ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
 /* ROUTES IMPORTING */
