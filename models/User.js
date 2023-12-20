e1703b1632cf6233e0056ad60bb8dae1c216f578
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-12-20 17:24:07 -0500

    User model created with function register for user registration

---
+++ models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: string,
+    occupation: string,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
