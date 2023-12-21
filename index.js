c62dbcffb366016c8dcaa5815fd122fd1e8f80e3
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2023-12-21 14:52:51 -0500

    import UserRoutes added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import bodyParser from "body-parser";
 import path from "path";
 import authRoutes from "./routes/auth";
 import { register } from "./controllers/auth";
+import userRoutes from "./routes/users.js";
 import { fileURLToPath } from "url";
 
 /* CONFIGURATIONS */
@@ -46,6 +47,7 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ROUTES */
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 
